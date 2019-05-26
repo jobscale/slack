699ed750d28fa062bc91deb7a8a748d766691af3
jobscale <jobscale@jsx.jp>
2019-05-26 20:12:16 +0900

    first

---
+++ README.md
@@ -0,0 +1,59 @@
+## install
+
+```
+npm i -S jobscale/slack
+```
+
+## example
+
+```
+const env = require('env');
+const { SpeedTest } = require('speedtest');
+const { Slack } = require('slack');
+const { Weather } = require('weather');
+const { DownDetector } = require('downdetector');
+const { Shell } = require('shell');
+
+const datas = [{
+  icon_emoji: ':email:',
+  username: 'Speed',
+  attachments: [{
+    fallback: '',
+  }],
+}];
+const getUrls = text => {
+  const search = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g;
+  return text.match(search);
+};
+const getData = text => {
+  const data = Object.assign({}, datas[0]);
+  const urls = getUrls(text);
+  data.text = text;
+  if (!urls) return data;
+  data.attachments = [{
+    fallback: '',
+    thumb_url: urls[0],
+  }];
+  return data;
+};
+const main = () => {
+  let delay = 1;
+  const slack = new Slack(env.slack);
+  const sender = param => setTimeout(
+    data => slack.send(data), (delay++) * 6000, getData(`${param.caption} - <${param.image}|icon>`),
+  );
+  let text;
+  new DownDetector().run()
+  .then(record => record.map(res => sender(res)))
+  .then(() => new Weather().run())
+  .then(res => sender(res))
+  .then(() => new Shell().spawn('curl', ['ifconfig.io']))
+  .then(res => slack.send(getData(res)))
+  .then(() => new SpeedTest().run())
+  .then(res => slack.send(getData(text = res)))
+  .then(() => logger.info(text));
+};
+(() => {
+  main();
+})();
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+require('common');
+const FormData = require('form-data');
+
+class Slack {
+  constructor(env) {
+    this.env = Object.assign({}, env);
+  }
+  send(param) {
+    const url = `https://hooks.slack.com/services/${this.env.access}`;
+    const body = JSON.stringify(param, null, 2);
+    const options = {
+      method: 'POST',
+      'Content-Type': 'application/json',
+      body,
+    };
+    return fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.text();
+    })
+    .catch(e => e.message);
+  }
+  getHistory(count) {
+    const url = 'https://slack.com/api/channels.history';
+    const param = Object.assign({}, this.env);
+    delete param.access;
+    const form = new FormData();
+    form.append('token', param.token);
+    form.append('channel', param.channel);
+    form.append('count', count);
+    const options = {
+      method: 'POST',
+      body: form,
+    };
+    return fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    });
+  }
+  spacer(delay) {
+    const promise = promiseGen();
+    setTimeout(promise.resolve, delay, { delay });
+    return promise.instance;
+  }
+  deleteMessage(message) {
+    const url = 'https://slack.com/api/chat.delete';
+    const param = Object.assign({}, this.env);
+    delete param.access;
+    const form = new FormData();
+    form.append('token', param.token);
+    form.append('channel', param.channel);
+    form.append('ts', message.ts);
+    const options = {
+      method: 'POST',
+      body: form,
+    };
+    return fetch(url, options)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(e => e.message)
+    .then(info => logger.info(info));
+  }
+  async deleteChat(messages) {
+    const param = { count: 0 };
+    for (let i = messages.length - 1; i > 100; i--) {
+      await this.deleteMessage(messages[i])
+      .then(() => this.spacer(800));
+      param.count++;
+    }
+    return { ok: param.count };
+  }
+}
+module.exports = {
+  Slack,
+};
